ae209c43c5befeae10a4411230aeaa21c3682f7b
karthick-thangaraju-15 <81950942+karthick-thangaraju-15@users.noreply.github.com>
2021-04-05 12:17:21 +0530

    Add files via upload

---
+++ vuejs/vuejs.js
@@ -0,0 +1,11 @@
+ 
+  var text=new Vue({  
+      el: '.text',  
+      data: {  
+        message: 'What Am I?' ,
+         message1: 'I am Creating my "VUE JS" Project',   
+        message2: 'by Karthick',
+        message3: 'More'   
+      }  
+    })  
+ 
